7a35350f045b7b046371dde6c0e476cabdb3a8e8
Juan_Prieto <gadaf.78@hotmail.com>
2020-09-16 11:16:53 -0500

    feat: Header Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../assets/styles/components/Header.scss'
 
 const Header = () => (
     <header className="header">
-        <img className="header__img" src="../assets/logo-platzi-video-BW2.png" alt="Platzi Video" />
+        <img className="header__img" src="../assets/images/logo-platzi-video-BW2.png" alt="Platzi Video" />
         <div className="header__menu">
             <div className="header__menu--profile">
-                <img src="../assets/user-icon.png" alt="" />
+                <img src="../assets/images/user-icon.png" alt="" />
                 <p>Perfil</p>
             </div>
             <ul>
