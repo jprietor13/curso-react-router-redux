339d403a67591e44dd1f3245f1fbdb165c8ff631
Juan_Prieto <gadaf.78@hotmail.com>
2020-09-16 14:52:32 -0500

    feat: Footer styles

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss'
 
@@ -19,6 +21,8 @@ const App = () => (
                 <CarouselItem />
             </Carousel>
         </Categories>
+
+        <Footer />
     </div>
 );
 
