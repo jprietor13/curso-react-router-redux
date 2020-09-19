a213c489fb45444f5fb5b3ffa2f6721f3c9a27f9
Juan_Prieto <gadaf.78@hotmail.com>
2020-09-19 16:10:41 -0500

    feat: Insert React hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from '../components/Header';
 import Search from '../components/Search';
@@ -6,38 +6,53 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
+    const [videos, setVideos] = useState([]);
 
-        <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    useEffect(() => { //funcion anonima
+        fetch(' http://localhost:3000/initalState')//recibe la api
+        .then(response => {// la api resuelve y regresa un json como respuesta
+            return response.json();
+        })
+        .then(data => {// la respuesta (data) que regresa el json, se pasa a la funcion(estado) setVideos
+            return setVideos(data);
+        });
+    }, []);// se pasa siempre un segundo valor como un array vacio
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    console.log(videos);
+    
+    return (
+        <div className="App">
+            <Header />
+            <Search />
 
-        <Categories title="Originales de platzi video">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+            <Categories title="Mi lista">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
 
-        <Footer />
-    </div>
-);
+            <Categories title="Tendencias">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Categories title="Originales de platzi video">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    )
+};
 
 export default App;
