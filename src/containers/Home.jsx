850f9ea7ea20553bd90539f1558036b384c62ebe
Juan_Prieto <gadaf.78@hotmail.com>
2020-09-26 10:17:18 -0500

    feat: Create first route

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,7 +12,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
     const inicialState = useInitialState(API);
     return (
         <div className="App">
@@ -54,4 +54,4 @@ const App = () => {
     )
 };
 
-export default App;
+export default Home;
