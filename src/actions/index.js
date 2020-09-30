186dfce5c8cdfc3cabff783a558248bee1218501
Juan_Prieto <gadaf.78@hotmail.com>
2020-09-30 14:57:29 -0500

    feat: Create Reducers

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+//en setFavorite se guarda ahi en favoritos la 
+//info que se transmitira mediante payload
+
+//type sera el nombre que identificaremos esta descripcion
+// de lo que estamos haciendo
+export const setFavorite = payload => ({
+    type: 'SET_FAVORITE',
+    payload,
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,14 @@
 const reducer = (state, action) => {
-    return state;
+    switch(action.type){
+        case 'SET_FAVORITE'://manejamos el set favorite
+            return {
+                ...state,//se trae el state destructurado
+                //elemento a actulizar
+                myList: [...state.myList, action.payload]  //array con los elementos que contenga y objeto que guardara
+            } 
+        default: //si no coindice con ninguno de los casod, mandar el estado siempre como se encontro
+            return state;
+    }
 }
 
 export default reducer;
