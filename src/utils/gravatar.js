72bf2f9c11a1d1f02dc933e90d4796481c5f5d5a
Juan_Prieto <gadaf.78@hotmail.com>
2020-10-01 15:09:58 -0500

    feat: Create gravatar

---
+++ src/utils/gravatar.js
@@ -0,0 +1,12 @@
+import md5 from 'md5';
+
+const gravatar = email => {
+    //url base de la imagen del gravatar
+    const base = 'https://gravatar.com/avatar/';
+    //formateamos el correo, eliminamos espacios y convertimos el texto a miniscula
+    const formatEmail = (email).trim().toLowerCase();
+    //creamis el hash, pasamos el correo formateado y el formato binario
+    const hash = md5(formatEmail, { encoding: "binary" });
+
+    return `${base}${hash}`;
+}
